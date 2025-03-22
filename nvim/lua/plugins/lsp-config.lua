38a965e7ae0395117b8450691dcf5a72c4b12c56
Nicolás Buero <nicolasbuero9@gmail.com>
2025-03-21 23:06:13 -0300

    remove completions, cleanup ruby lsp config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -17,20 +17,8 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
       local lspconfig = require("lspconfig")
-      lspconfig.ruby_lsp.setup({
-        capabilities = capabilities
-      })
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>v", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", {})
-      vim.keymap.set("n", "<leader>r", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+      lspconfig.ruby_lsp.setup({})
     end,
   },
 }
