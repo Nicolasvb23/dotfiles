11e8007164aa67023667934d581244fc53b006a1
Nicolás Buero <nicolasbuero9@gmail.com>
2025-03-29 00:30:04 -0300

    removing unused keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -12,13 +12,9 @@ vim.keymap.set('n', '<C-.>', ':+tabnext<CR>')
 vim.api.nvim_set_keymap('n', '<leader>cp', [[:let @+=expand('%')<CR>]], { noremap = true, silent = true })
 
 -- LSP keymaps
-vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "<leader>v", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", {})
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.references, {})
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
 
 -- Snacks keymaps
 local opts = { hidden = true }
